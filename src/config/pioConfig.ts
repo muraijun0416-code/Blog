5d9da98b7739f1f6aaeba8c553a0bff607e87101
村井宏行 <muraihiroyuki@MacBook-Air-2.local>
2026-08-31 22:35:43 +0900

    Fix build issues

--- src/config/pioConfig.ts
+++ src/config/pioConfig.ts
@@ -1,8 +1,8 @@
 import type { PioConfig } from "../types/config";
 
 // Pio 看板娘配置
-/*export const pioConfig: PioConfig = {
-	enable: true, // 启用看板娘
+export const pioConfig: PioConfig = {
+	enable: false, // 启用看板娘
 	models: ["/pio/models/NOIR/noir.model3.json"], // 默认模型路径
 	position: "left", // 模型位置
 	width: 280, // 默认宽度
@@ -23,4 +23,4 @@ import type { PioConfig } from "../types/config";
 		close: "QWQ See you next time~", // 关闭提示
 		link: "https://github.com/LyraVoid/Mizuki", // 关于链接
 	},
-};**/
+};
--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "ja"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 export const siteConfig: SiteConfig = {
 	title: "Mu",
 	subtitle: "A website",
-	siteURL: "", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://example.com", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-08-30", // 站点开始运行日期，用于站点统计组件计算运行天数
 	timeZone: "Asia/Tokyo", // 文章日期使用的 IANA 时区，可改为 Asia/Tokyo、Europe/Berlin 等
 
